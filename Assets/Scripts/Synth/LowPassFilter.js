9cee8b8465bfd07dec969bccedd52a486c11de28
keijiro <keijiro@gmail.com>
2012-08-04 22:52:53 +0900

    Added some guard functions.

--- Assets/Scripts/Synth/LowPassFilter.js
+++ Assets/Scripts/Synth/LowPassFilter.js
@@ -20,22 +20,26 @@ class LowPassFilter {
     var resonance = 0.0;
     var envMod = 0.0;
     
+    private function Saturate(x : float) {
+        return Mathf.Clamp(x, -1.0, 1.0);
+    }
+    
     function LowPassFilter(anEnv : Envelope) {
         env = anEnv;
     }
     
     function Run(input : float) {
         var f = Mathf.Clamp01(cutoff + env.current * envMod) * 1.16;
-        var fb = resonance * (1.0 - 0.15 * f * f);
+        var fb = Mathf.Clamp01(resonance) * 4.0 * (1.0 - 0.15 * f * f);
         var x = input - o4 * fb;
-        x *= 0.35013 * (f * f) * (f * f);
-        o1 = x + 0.3 * i1 + (1.0 - f) * o1;
+        x = Mathf.Clamp(0.35013 * x * (f * f) * (f * f), -1.0, 1.0);
+        o1 = Saturate(x + 0.3 * i1 + (1.0 - f) * o1);
         i1 = x;
-        o2 = o1 + 0.3 * i2 + (1.0 - f) * o2;
+        o2 = Saturate(o1 + 0.3 * i2 + (1.0 - f) * o2);
         i2 = o1;
-        o3 = o2 + 0.3 * i3 + (1.0 - f) * o3;
+        o3 = Saturate(o2 + 0.3 * i3 + (1.0 - f) * o3);
         i3 = o2;
-        o4 = o3 + 0.3 * i4 + (1.0 - f) * o4;
+        o4 = Saturate(o3 + 0.3 * i4 + (1.0 - f) * o4);
         i4 = o3;
         return o4;
     }
